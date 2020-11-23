b20c7e5442ef15f14638cefb0a37dcde0602de40
CabAlexis <alexis.cabillic@gmail.com>
2020-11-23 15:35:45 +0100

    Mise en place de la structure

--- public/index.php
+++ public/index.php
@@ -1,23 +1,10 @@
 <?php if (!@include_once('../vendor/autoload.php')) echo "You must install package to start app.";
 
-
-
+$templateName = '';
 if (!empty($_GET['page'])) {
-    $page = trim($_GET['page']);
+    $templateName = trim($_GET['page']);
 }
 else {
-    $page = 'presentation';
-}
-$templateName = '';
-
-if ($page == 'projets') {
-    $templateName = 'projets';
-}
-else if ($page == 'experience') {
-    $templateName = 'experience';
-}
-else if ($page == 'presentation') {
-
     $templateName = 'presentation';
 }
 
